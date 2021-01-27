d8d56fe0ac88c9e4701834e45313525e46d75052
LeticiaCMZ <leticmz@gmail.com>
2021-01-27 20:35:13 -0300

    adiciona pasta js e o index

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var sum0ld = function(a, b) {
+
+    return a + b;
+};
+
+//Arrow Functions 
+var sum = (a, b) => {
+
+    var x = 10;
+    if (a > b) {
+
+    }
+    return a + b;
+}
+
+//var sum = (a, b) => a + b;
+console.log(sum(5, 15));
+
+/*
+                            var log = function(value) {
+
+                            console.log(value);
+                            };
+
+                            log('test');
+                            */
+
+/* var meunumero = 20;
+                           var meutexto = ' exemplo';
+
+                           console.log(meunumero + meutexto);
+                           */
